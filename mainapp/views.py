d5c3dd980639691a0586fbab8731b77dac52af41
drishtipeshwani <archiepeshwani09@gmail.com>
2023-02-21 02:39:58 +0530

    Updated summarising function

--- mainapp/views.py
+++ mainapp/views.py
@@ -182,24 +182,25 @@ def video(request,moduleName,videoId):
     return render(request,'video.html',{"videoObject":videoObject,"title":moduleName})
 
 def summarise(text):
-    model = T5ForConditionalGeneration.from_pretrained('t5-small')
-    tokenizer = T5Tokenizer.from_pretrained('t5-small')
-    device = torch.device('cpu')
-
-    preprocess_text = text.strip().replace("\n","")
-    t5_prepared_Text = "summarize: "+preprocess_text
-    tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt",truncation=True).to(device)
-
-    # summmarize 
-    summary_ids = model.generate(tokenized_text,
+    output = None
+    try:
+        model = T5ForConditionalGeneration.from_pretrained('t5-small')
+        tokenizer = T5Tokenizer.from_pretrained('t5-small')
+        device = torch.device('cpu')
+        preprocess_text = text.strip().replace("\n","")
+        t5_prepared_Text = "summarize: "+preprocess_text
+        tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt",truncation=True).to(device)
+        # summmarize 
+        summary_ids = model.generate(tokenized_text,
                                     num_beams=4,
                                     no_repeat_ngram_size=2,
                                     min_length=30,
                                     max_length=3000,
                                     early_stopping=True)
 
-    output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-
+        output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    except:
+        output = "Unable to generate summary currently"
     return output
 
     
